5695261aa81156a4eb278e27c11c90e708ba60a1
Ayushshar11 <ayushsharmabros@gmail.com>
2026-01-08 14:37:21 +0530

    Delete assignments1.js

--- assignments1.js
+++
@@ -1,9 +0,0 @@
-const rawUsers = [
-{ id: 1, name: "Rahul", password: "fb_password", role: "admin" },
-{ id: 2, name: "Sanya", password: "123_password", role: "user" },
-{ id: 3, name: "Amit", password: "secret_password", role: "user" }
-];
-const users=rawUsers.map(({password,...rest})=>rest);
-console.log(users);
-const final_result = users.filter(user => user.role === "admin");
-console.log(final_result);
